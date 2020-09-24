38c2d5f3fa82084a15ca509e2aea5896be079ccd
unknown <izra.arzi@gmail.com>
2020-09-24 13:05:59 +0700

    fix product spacing

--- client/src/views/Products.vue
+++ client/src/views/Products.vue
@@ -3,12 +3,12 @@
     <h1 class="justify-content-center">PRODUCTS</h1>
     <div class="container mt-3">
       <div class="justify-content-center">
-        <router-link :to="`/products/1`"><img class="mr-4" src="../assets/consumables.png" width="35px"></router-link>
-        <router-link :to="`/products/2`"><img class="mr-4" src="../assets/accessories.png" width="35px"></router-link>
-        <router-link :to="`/products/3`"><img class="mr-4" src="../assets/support.png" width="35px"></router-link>
-        <router-link :to="`/products/4`"><img class="mr-4" src="../assets/magical.png" width="35px"></router-link>
-        <router-link :to="`/products/5`"><img class="mr-4" src="../assets/armor.png" width="35px"></router-link>
-        <router-link :to="`/products/6`"><img class="mr-4" src="../assets/weapons.png" width="35px"></router-link>
+        <router-link :to="`/products/1`" class="mr-4"><img src="../assets/consumables.png" width="35px"></router-link>
+        <router-link :to="`/products/2`" class="mr-4"><img src="../assets/accessories.png" width="35px"></router-link>
+        <router-link :to="`/products/3`" class="mr-4"><img src="../assets/support.png" width="35px"></router-link>
+        <router-link :to="`/products/4`" class="mr-4"><img src="../assets/magical.png" width="35px"></router-link>
+        <router-link :to="`/products/5`" class="mr-4"><img src="../assets/armor.png" width="35px"></router-link>
+        <router-link :to="`/products/6`" class="mr-4"><img src="../assets/weapons.png" width="35px"></router-link>
         <router-link :to="`/products/7`"><img src="../assets/artifacts.png" width="35px"></router-link>
       </div>
       <router-view/>
