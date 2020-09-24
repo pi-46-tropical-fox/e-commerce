deb2d10e02f8b76046e1bbae70d1b0b617ef3da0
mariosupit <armandosupit@gmail.com>
2020-09-24 12:31:43 +0700

    ready to deploy

--- server/app.js
+++ server/app.js
@@ -8,11 +8,11 @@ const cors = require('cors')
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cors());
+app.use(router);
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 })
 
-app.use(router)
 
 module.exports = app
