dd504425064048f16fb641358e82ccc41c2b6b79
ArdySitio <ardy.ai29@gmail.com>
2020-09-24 09:52:29 +0700

    Change procfile

--- server/app.js
+++ server/app.js
@@ -2,8 +2,10 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const routes = require('./routes')
+const port = process.env.PORT || 3001
 const cors = require('cors')
 const errorHandler = require('./middlewares/errHandler')
+
 
 
 app.use(cors())
@@ -15,5 +17,8 @@ app.use('/', routes)
 
 app.use(errorHandler)
 
+app.listen(port, () => {
+    console.log(`App listen on port: ${port}`);
+})
 
 module.exports = app
