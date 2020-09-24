00e8fd684d8314464f63eaf3437d287e0e68dcee
bryandaniswara <bryan.daniswara@gmail.com>
2020-09-24 10:09:31 +0700

    Fix typo in baseURL Axios

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://e-commerce-cms-bdh8.herokuapp.com/',
+    baseURL: 'https://e-commerce-cms-bdh8.herokuapp.com',
 });
 
 export default instance;
