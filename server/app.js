4b74ef8aa895b803cc0ea40f57c28c5264137c63
abdulfattah94 <abdulfattah.1994@gmail.com>
2020-09-24 12:18:54 +0700

    test

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ if(process.env.NODE_ENV === 'development'){
 }
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const router = require('./routes')
 const errHandler = require('./middlewares/errHandler')
 const cors = require('cors')
