e7ee77a4048155b86160121c0d546b81e6c14dba
syukurbaru <syukurbaru@gmail.com>
2020-09-24 10:07:13 +0700

    handle root /

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,9 @@ const routeUser = require('./user')
 const routeProduct = require('./product')
 const routeCart = require('./cart')
 
+router.get('/', (req,res) => {
+    res.send('Server Ecommerce Syukur')
+})
 router.use('/', routeUser)
 router.use('/products', routeProduct)
 router.use('/carts', routeCart)
