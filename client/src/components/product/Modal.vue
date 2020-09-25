ea367ab243fa9c509557c0f98dd498c74f9d3e5b
Eas <me@eas.web.id>
2020-09-25 13:55:47 +0700

    bug fix on image checking

--- client/src/components/product/Modal.vue
+++ client/src/components/product/Modal.vue
@@ -22,7 +22,6 @@
       <div class="modal-content">
         <!-- Title -->
         <div class="modal-title">
-          <span class="title">{{ data.name }}</span>
         </div>
 
         <!-- Hidden Content(s) -->
@@ -30,7 +29,7 @@
         <!-- Content -->
         <div class="grid grid-cols-2 gap-x-4">
           <div class="product-images">
-            <div v-if="typeof data.image == 'String'" 
+            <div v-if="!Array.isArray(data.image)" 
               class="bg-gray-600 text-white flex justify-center items-center h-64"
             >
               <h4>No image available</h4>
@@ -43,7 +42,13 @@
             </div>
           </div>
           <div class="product-details">
-            {{ data }}
+
+            <h3 class="title">{{ data.name }}</h3>
+            <h4 class="text-red-500">IDR {{ (data.price / 100).toLocaleString('id') }}</h4>
+
+            <h4>Stock remaining: {{ data.stock }}</h4>
+            <h5>Filled in: {{ data.category }}</h5>
+            <!-- {{ data }} -->
           </div>
         </div>
         <hr />
@@ -71,16 +76,6 @@ export default {
     showModal: Boolean,
   },
 
-  data() {
-    return {
-      name: "",
-      price: "",
-      stock: "",
-      images: [""],
-      CategoryId: "",
-    };
-  },
-
   components: {},
 
   computed: {
@@ -89,7 +84,7 @@ export default {
     },
 
     imageAvailable() {
-      let image = this.$store.state.productDetails
+      let image = !!this.$store.state.productDetails.image
       return image
     }
   },
@@ -118,90 +113,4 @@ export default {
 </script>
 
 <style>
-.card {
-  @apply shadow-lg;
-  /* @apply w-auto; */
-  @apply rounded-lg;
-
-  min-width: min-content;
-}
-
-.modal {
-  @apply fixed;
-  @apply w-full h-full;
-  @apply top-0 left-0;
-  @apply flex;
-  @apply items-center justify-center;
-  @apply overflow-y-auto;
-
-  @apply transition-opacity duration-200;
-}
-
-.modal-overlay {
-  @apply fixed;
-  @apply w-full h-full;
-  @apply bg-gray-900;
-  @apply opacity-25;
-}
-
-.modal-container {
-  @apply bg-white;
-  @apply w-11/12;
-  @apply mx-auto;
-  @apply card;
-  @apply shadow-lg;
-  @apply z-50;
-}
-
-.modal-close {
-  top: 3.5em;
-  @apply fixed;
-  @apply right-0;
-  @apply cursor-pointer;
-  @apply flex flex-col;
-  @apply items-center;
-  @apply mt-4 mr-4;
-  @apply text-white text-sm;
-  @apply z-50;
-}
-
-.modal-content {
-  @apply py-4 px-6;
-  @apply text-left;
-}
-
-.modal-title {
-  @apply flex;
-  @apply justify-between items-center;
-  @apply pb-3;
-}
-
-.modal-title > .title {
-  @apply text-2xl;
-  @apply font-bold;
-}
-
-.modal-footer {
-  @apply flex;
-  @apply justify-end;
-  @apply pt-2;
-}
-
-button {
-  @apply py-2;
-  @apply px-4;
-  @apply rounded;
-  @apply mx-1;
-  @apply text-white;
-}
-
-button.cart {
-  @apply bg-green-500;
-}
-button.details {
-  @apply bg-blue-500;
-}
-button.wishlist {
-  @apply bg-red-500;
-}
 </style>
